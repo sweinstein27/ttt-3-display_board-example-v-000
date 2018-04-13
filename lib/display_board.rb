2cc440c20c6935a01d4468af7830a4c091e4400d
Scott Weinstein <sweinstein27@gmail.com>
2018-04-13 19:15:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board_row
+def display_board
   return "   |   |   "
   return "-----------"
+  return "   |   |   "
+  return "-----------"
+  return "   |   |   "
 end
 
-  def display_board
-    return display_board_row
-    return display_board_row
-    return "   |   |   "
-  end
-  
